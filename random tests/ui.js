ff52fda4b7ae6448ff43b340e638fd5bf64240b8
Tokarchukm <40299139+Tokarchukm@users.noreply.github.com>
2019-11-12 03:59:35 +0200

    Delete ui.js

--- random tests/ui.js
+++
@@ -1,7 +0,0 @@
-import { t, ClientFunction } from 'testcafe'
-
-
-export const getSsid = ClientFunction( () => {
-    const ssidRegExp = /ssid/gm
-    return document.cookie.split(';').find(elem=> ssidRegExp.test(elem))
-})
